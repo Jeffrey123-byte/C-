434572bc39129ea508802f55894ace9b1fff977b
Jeffrey123-byte <jeffreyjebastin@gmail.com>
2025-08-14 09:23:19 +0530

    Day 3 submission

---
+++ Day 3/Program.cs
@@ -0,0 +1,51 @@
+﻿//// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+//Console.WriteLine("\n");
+
+//// if else
+//bool age = false;
+//string result = (age == true) ? "the world is shiny" : "The world is filled with grim reapers";
+//Console.WriteLine(result);
+
+//Console.WriteLine("\n");
+
+////for loop
+//for (int i = 0; i < 20; ++i) {
+//    Console.Write(i + " ");
+//}
+
+
+//Console.WriteLine("\n");
+
+//int j = 0;
+//while( j <= 5){ 
+//    j++;
+//    Console.Write(j+" ");
+//}
+
+//Console.WriteLine("\n");
+
+//int k = 0;
+//do
+//{
+//    ++k;
+//    Console.Write(k + " " + ++k + "  " );
+
+//} while (k < 5);
+
+//Console.WriteLine("\n");    
+//int[] nums = [5,6,7,8,9];
+
+//foreach (int num in nums)
+//    Console.WriteLine(num);
+
+//for (int i = 0; i < nums.Length; i++) {
+//    Console.WriteLine(i);
+//}
+
+for (int i = 0; i < 5; ++i) {
+    
+    Console.Write(++i);
+    
+     }
